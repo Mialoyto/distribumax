893731d72fb5d32e516ce255a297294f984c27ca
Miguel Loyola <mialoyto1999@gmail.com>
2024-09-11 13:49:46 -0500

    se inserto un tag ul para renderizar los datos

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="<?=$host?>/js/scripts.js"></script>
-<script src="<?=$host?>/js/documento.js"></script>
-<script src="<?=$host?>/js/persona.js"></script>
+<!-- <script src="<?=$host?>/js/persona.js"></script> -->
+<script src="<?=$host?>/js/usuarios.js"></script>
 
 

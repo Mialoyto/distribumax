7739bcd2c725df7ab3e57d4b72fbac64511d8acf
jcastilla04 <javierqatar234@gmail.com>
2024-09-15 22:48:43 -0500

    No avance mucho

--- js/cliente.js
+++ js/cliente.js
@@ -1,7 +1,42 @@
 document.addEventListener("DOMContentLoaded", () =>{
   const persona = document.querySelector("#idpersona");
-  const empresa = document.querySelector("idempresa");
-  const tipoCliente = document.querySelector("tipo_cliente");
+  const empresa = document.querySelector("#idempresa");
+  const tipoCliente = document.querySelector("#tipo_cliente");
+  const contentable = document.querySelector("#table-clientes tbody");
 
+  (() =>{
+    fetch(`../../controller/cliente.controller.php?operation=getAll`)
+        .then(response => response.json())
+        .then(data => {
+          data.forEach(element => {
+            const tagOption = document.createElement('option');
+            tagOption.value = element.idpersona;
+            tagOption.innerText = element.cliente;
+            persona.appendChild(tagOption);
+          });
+        })
+        .catch(e => {
+          console.error(e);
+        })
+  })();
+
+(() =>{
+  fetch(`../../controller/cliente.controller.php?operation=getAll`)
+      .then(response=>response.json())
+      .then(datos=>{
+        datos.forEach(row=>{
+          contentable.innerHTML+= `
+          <tr>
+              <td>${row.idpersona}</td>
+              <td>${row.idempresa}</td>
+              <td>${row.tipoCliente}</td>
+          </tr>
+          `
+        })
+      })
+      .catch(e=>{
+        console.error(e);
+      })
+})();
   
 })

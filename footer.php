da9e3d1bb9a80b184b71b6a71be7fb1ad8c25114
Miguel Loyola <mialoyto1999@gmail.com>
2024-09-18 14:43:39 -0500

    se agrego cdn de bootstrap bundle

--- footer.php
+++ footer.php
@@ -13,6 +13,3 @@
 <script src="<?= $host ?>/js/scripts.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-<!-- <script src="<?= $host ?>/js/persona.js"></script> -->
-
-<!-- <script src="<?= $host ?>/js/persona.js"></script> -->

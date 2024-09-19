799506e252a861c523074bd2e996fd3b6db0774e
Miguel Loyola <mialoyto1999@gmail.com>
2024-09-19 13:04:46 -0500

    update detalle pedidos

--- db/15.  DetallePedidos.sql
+++ db/15.  DetallePedidos.sql
@@ -1,15 +1,15 @@
 USE distribumax;
 
--- REGISTRAR DETALLE PROMOCIONES
+-- REGISTRAR DETALLE PEDIDOS
 DELIMITER $$
 CREATE PROCEDURE sp_detalle_pedido(
-    IN _idpedido INT,
-    IN _idproducto INT,
-    IN _cantidad_producto INT,
-    IN _unidad_medida INT,
-    IN _precio_unitario DECIMAL(8, 2),
-    IN _precio_descuento DECIMAL(8, 2),
-    IN _subtotal DECIMAL(8, 2)
+    IN _idpedido            INT,
+    IN _idproducto          INT,
+    IN _cantidad_producto   INT,
+    IN _unidad_medida       INT,
+    IN _precio_unitario     DECIMAL(10, 2),
+    IN _precio_descuento    DECIMAL(10, 2),
+    IN _subtotal            DECIMAL(10, 2)
 )
 BEGIN
     INSERT INTO detalle_pedidos 
@@ -19,41 +19,35 @@ BEGIN
 END$$
 
 -- ACTUALIZAR DETALLE PEDIDOS
-/** Se puede actualizar algun campo del proceso de actualizar**/
+/** Se puede actualizar algun campo del proceso de actualizar PROBAR SI SE PUEDE ACTUALIZAR EL PRECIO UNITARIO**/
 DELIMITER $$
+
 CREATE PROCEDURE sp_actualizar_detalle_pedido(
     IN _idpedido          INT,
     IN _idproducto        INT,
     IN _cantidad_producto INT,
-    IN _unidad_medida     INT,
-    IN _precio_unitario   DECIMAL(8, 2),
-    IN _precio_descuento  DECIMAL(8, 2),
-    IN _subtotal          DECIMAL(8, 2),
     IN _iddetallepedido   INT
 )
 BEGIN
     UPDATE detalle_pedidos
         SET 
-            idpedido=           _idpedido,
-            idproducto=         _idproducto,
-            cantidad_producto=  _cantidad_producto,
-            unidad_medida=      _unidad_medida,
-            precio_unitario=    _precio_unitario,
-            precio_descuento=   _precio_descuento,
-            subtotal=           _subtotal,
-            update_at=          now()
-        WHERE iddetallepedido=  _iddetallepedido;
+            idpedido            = _idpedido,
+            idproducto          = _idproducto,
+            cantidad_producto   = _cantidad_producto,
+            update_at           = now()
+        WHERE iddetallepedido   = _iddetallepedido;
 END$$
 
 -- ESTADO DETALLE PEDIDOS
 /** Debe de poder eliminar el pedido **/
 DELIMITER $$
+
 CREATE PROCEDURE sp_estado_detalle_pedido(
     IN  _estado           CHAR(1),
     IN  _iddetallepedido  INT 
 )
 BEGIN
     UPDATE detalle_pedidos SET
-        estado=_estado
-        WHERE iddetallepedido=_iddetallepedido;
+        estado = _estado
+        WHERE iddetallepedido = _iddetallepedido;
 END$$

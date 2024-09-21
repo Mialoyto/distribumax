5da0a0d1e14a5ef8fe79e135d9e669ff531c9312
Miguel Loyola <mialoyto1999@gmail.com>
2024-09-20 23:00:41 -0500

    trabajando en pedido y cliente

---
+++ model/Pedido.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Pedidos extends Conexion
+{
+  private $pdo;
+  public function __construct()
+  {
+    $this->pdo = Conexion::getConexion();
+  }
+
+  // funcion para agregar pedido
+  public function agregarPedido($params = []): int
+  {
+    $id = -1;
+    try {
+
+      $sql = "CALL sp_pedido_registrar( ?, ?)";
+      $cmd = $this->pdo->prepare($sql);
+      $cmd->execute(array(
+        $params['idusuario'],
+        $params['idcliente']
+      ));
+      $row = $cmd->fetch(PDO::FETCH_ASSOC);
+      $id = $row['idpedido'];
+    } catch (Exception $e) {
+      // $this->pdo->rollBack();
+      // echo $e->getMessage();
+      return $id;
+    }
+    return $id;
+  }
+}

b501847ba48c10c92d2a9fc7dba4d9d34d632157
jcastilla04 <javierqatar234@gmail.com>
2024-11-14 00:57:39 -0500

    Avances de promociones

--- views/Promociones/registrar.php
+++ views/Promociones/registrar.php
@@ -1,6 +1,4 @@
-<?php
-require_once '../../header.php';
-?>
+<?php require_once '../../header.php'; ?>
 <div class="container mt-5">
     <!-- Formulario Completo -->
     <div class="card shadow-lg border-0 rounded-lg">
@@ -48,8 +46,44 @@ require_once '../../header.php';
                     <a href="index.php" class="btn btn-outline-primary">Listar promociones</a>
                 </div>
             </form>
+
+            <div class="text-end mt-4">
+                <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalTipoPromocion">Registrar Tipo de Promoción</button>
+            </div>
         </div>
     </div>
 </div>
+
+<!-- Modal para Registrar Tipo de Promoción -->
+<div class="modal fade" id="modalTipoPromocion" tabindex="-1" aria-labelledby="modalTipoPromocionLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalTipoPromocionLabel">Registro de Tipo de Promoción</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="#" id="form-promocion">
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="tipopromocion" class="form-label">Tipo de Promoción</label>
+                            <input type="text" class="form-control" id="tipopromocion" name="tipopromocion" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="descripcion" class="form-label">Descripción</label>
+                            <input type="text" class="form-control" id="descripcion" name="descripcion_tipo" required>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary me-2">Registrar Tipo</button>
+                        <button type="reset" class="btn btn-secondary">Cancelar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="../../js/tipopromociones/registrar.js"> </script>
 <script src="../../js/promociones.js"> </script>
-<?php require_once '../../footer.php';?>
+<?php require_once '../../footer.php'; ?>
